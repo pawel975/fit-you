608978bd5ee1bcf91c7e1c74b991bc5983739548
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-02-04 20:31:59 +0100

    build fix

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ btn.forEach(item => {
     })
 })
 
-// const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY;
 
 let state;
 
-const baseURL = `https://api.nal.usda.gov/fdc/v1/foods/search?query=cheddar%20cheese&dataType=Branded&pageSize=20&pageNumber=1&sortBy=publishedDate&sortOrder=desc&api_key=${API_KEY}`
+const baseURL = `https://api.nal.usda.gov/fdc/v1/foods/search?query=cheddar%20cheese&dataType=Branded&pageSize=20&pageNumber=1&sortBy=publishedDate&sortOrder=desc&api_key=${apiKey}`
 
 fetch(baseURL).then(response => response.json()).then(data => {
     // Object.assign(state, data.foods);
