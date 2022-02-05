c5cfa202dbd903e88d97ac3eb362a10abdbefa0f
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-02-05 20:16:24 +0100

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -14,14 +14,16 @@ module.exports = {
       }
     ]
   },
-  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: './',
+    open:true
+  },
   plugins: [
-    // plugin to store API key in .env file
       new Dotenv({
         prefix: 'process.env.'
       }),
