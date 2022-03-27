0f79e353065e245689dd087ec72bd0ff5941c79e
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-03-27 17:40:01 +0200

    fix background color bug on dark mode with mozilla

--- src/js/sideMenu.js
+++ src/js/sideMenu.js
@@ -1,27 +1,27 @@
 import { DOMelements } from "./base";
 import { trapFocus } from "./trapFocus";
 
+const {sideMenu, sideMenuClose, sideMenuOpen, sideMenuContent, sideMenuBg, mobileMenuOptions} = DOMelements;
+
+// Opens side menu
+const handleSideMenuOpen = () => {
+    sideMenu.style.left = "0%";
+    sideMenu.style.display = "initial";
+    sideMenu.style.visibility = "visible";
+    sideMenuContent.style.left = "0%";
+    document.body.classList.add("disable-scroll");
+    trapFocus(sideMenu)
+}
+
+const handleSideMenuClose = () => {
+    sideMenu.style.left = "-100%";
+    sideMenu.style.display = "none";
+    sideMenu.style.visibility = "hidden";
+    sideMenuContent.style.left = "-70%";
+    document.body.classList.remove("disable-scroll");
+}
+
 window.addEventListener("load", () => {
-
-    const {sideMenu, sideMenuClose, sideMenuOpen, sideMenuContent, sideMenuBg, mobileMenuOptions} = DOMelements;
-
-    // Opens side menu
-    const handleSideMenuOpen = () => {
-        sideMenu.style.left = "0%";
-        sideMenu.style.display = "initial";
-        sideMenu.style.visibility = "visible";
-        sideMenuContent.style.left = "0%";
-        document.body.classList.add("disable-scroll");
-        trapFocus(sideMenu)
-    }
-
-    const handleSideMenuClose = () => {
-        sideMenu.style.left = "-100%";
-        sideMenu.style.display = "none";
-        sideMenu.style.visibility = "hidden";
-        sideMenuContent.style.left = "-70%";
-        document.body.classList.remove("disable-scroll");
-    }
 
     sideMenuOpen.addEventListener("click", () => {
         handleSideMenuOpen()
