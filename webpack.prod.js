008eefd8dbf7ba4fa111395e8374bd8c1da7c862
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-04-01 09:57:43 +0200

    update wbepack config files

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   // source-map ussed for benchmark test in production, could be excluded 
-  devtool: 'source-map',
+  // devtool: 'source-map',
   performance: {
     hints: false,
   }
